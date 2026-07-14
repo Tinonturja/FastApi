ed5cb6ecbb6148b244abbb31a18c65dcbd9d9dd4
tinonturja <tinonturja@gmail.com>
2026-07-14 17:09:03 +0600

    Practicing the get method in fastapi

--- fitst_fastapi_web2.py
+++ fitst_fastapi_web2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 from pydantic import BaseModel, Field
 from typing import Annotated, List, Dict
@@ -30,5 +30,25 @@ def get_patient(patient_id):
     
     return data[patient_id]
 
+@app.get("/specificview/{patient_id}")
+def specific_view(patient_id: str = Path(..., description = "The specific patient id to view the data")):
+    data = load_data()
+    patient_id_list = [id for id in data.keys()]
+    if patient_id not in patient_id_list:
+        raise HTTPException(status_code = 404, detail = f"Patient ID is not in the database. Submit the id within this list {patient_id_list}")
+    
+    return data[patient_id]
 
+@app.get("/sort")
+def sort_patient(sort_by: str = Query(..., description = "The criteria to sort by: height, weight, and bmi"),
+                 order_by: str = Query('asc', description = "The order to sort by: asc or desc") ):
+    data = load_data()
+    valid_fields = ['height', 'weight', 'bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=404, detail=f"The data should be sory by the following fields: {valid_fields}")
+    if order_by not in ['asc', 'desc']:
+        raise HTTPException(status_code=404, detail= "order should follow two method: either ascending or descending")
+    reverse_option = True if order_by == 'desc' else False
+    data = sorted(data.values(), key = lambda x: x.get(sort_by,0), reverse= reverse_option)
 
+    return data
